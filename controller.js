d9201b0810c02b62f0931f3f15d20b9b98fa4c1c
Derek <derekphung16@gmail.com>
2022-12-31 18:08:37 -0500

    Did I successfully do it on my own ish?

--- controller.js
+++ controller.js
@@ -1,7 +1,9 @@
-document.getElementById("outsideBox").innerHTML += `
-    <input id="item" type="name" placeholder="Item you spent money on"></input><br>
-    <input id="date" type="date"></input>
-    $<input id="amount" type="number" min="0.01" placeholder="Amount"></input>
+thePage();
+function thePage(){
+    document.getElementById("outsideBox").innerHTML = `
+    <input id="item" type="name" placeholder="Item you spent money on"/><br>
+    <input id="date" type="date"/>
+    $<input id="amount" type="number" min="0.01" placeholder="Amount"/>
     <button onclick="addExpense()">Add Expense</button>
     <table id="IDK">
         <tr>
@@ -10,18 +12,47 @@ document.getElementById("outsideBox").innerHTML += `
             <th>Amount:</th>
         </tr>
     </table>
+
+    <table id="total">
+        <tr>
+            <th>Total Amount:</th>
+        </tr>
+    </table>
+    
+    <button onclick="removeExpense()">Remove</button>
 `;
+}
+
+let totalAmount = 0;
 
 function addExpense() {
     document.getElementById("IDK").innerHTML += `
         <tr>
             <td>${item.value}</td>
             <td>${date.value}</td>
-            <td>$${amount.value}</td>
+            <td>$${document.getElementById("amount").value}</td>
         </tr>
     `;
+
+    totalAmount = totalAmount + parseFloat(amount.value);
+    // let storage = parseFloat(amount.value);
+
+document.getElementById("total").innerHTML = `
+    <tr>
+        <td>Total Amount:$${totalAmount}</td>
+    </tr>
+`
 }
 
 function removeExpense(){
-    
+    // document.getElementById("IDK").innerHTML = `   
+    // <table id="IDK">
+    //     <tr>
+    //         <th>Item:</th>
+    //         <th>Date:</th>
+    //         <th>Amount:</th>
+    //     </tr>
+    // </table>`;
+    totalAmount = 0;
+    thePage();
 }
